e56ab186fbd5ddc8cc6ceaac94a11d90d2f83c9b
Barnabás Birmacher <birmacher@Barnabass-MacBook-Pro.local>
2018-07-23 21:05:04 +0200

    handlers

--- main_handlers.go
+++ main_handlers.go
@@ -2,13 +2,71 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+
+	"github.com/gorilla/mux"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-/* Provision Handler
+// ProvisionData ...
+type ProvisionData struct {
+	Username string `json:"username"`
+	Plan     string `json:"plan"`
+}
+
+// Environments ...
+type Environments struct {
+	Envs []Env `json:"envs"`
+}
+
+// Env ...
+type Env struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randomString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func generateUniqueToken() string {
+	token := randomString(24)
+
+	db, err := sql.Open("sqlite3", "./addon.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT count(*) FROM users WHERE token=?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	var output int
+	err = stmt.QueryRow(token).Scan(&output)
+	if err != nil {
+		panic(err)
+	}
+
+	if output == 0 {
+		return token
+	}
+	return generateUniqueToken()
+}
+
+/* postProvision
 Request body:
 {
 	"plan": "free",
@@ -37,51 +95,119 @@ Response body:
 	}
 */
 func postProvision(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("postProvision called")
+	decoder := json.NewDecoder(r.Body)
+
+	var provisionData ProvisionData
+	err := decoder.Decode(&provisionData)
+	if err != nil {
+		http.Error(w, "Failed to parse request", 500)
+	}
 
 	db, err := sql.Open("sqlite3", "./addon.db")
 	if err != nil {
-		panic(err)
+		http.Error(w, "Application error", 500)
 	}
-
 	defer db.Close()
 
-	stmt, err := db.Prepare("INSERT INTO users(username, plan) values(?,?)")
+	stmt, err := db.Prepare("INSERT INTO users(username, plan, token) values(?,?,?)")
 	if err != nil {
-		panic(err)
+		http.Error(w, "Failed to create user", 500)
+	}
+	defer stmt.Close()
+
+	token := generateUniqueToken()
+	_, err = stmt.Exec(provisionData.Username, provisionData.Plan, token)
+	if err != nil {
+		http.Error(w, "Failed to create user", 500)
 	}
 
-	_, err = stmt.Exec("username", "plan")
-	if err != nil {
-		panic(err)
+	envs := Environments{
+		Envs: []Env{},
 	}
+	env := Env{
+		Key:   "token",
+		Value: token,
+	}
+	envs.Envs = append(envs.Envs, env)
+
+	payload, err := json.Marshal(envs)
+	if err != nil {
+		http.Error(w, "Failed to generate payload", 500)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
 }
 
+/* putProvision
+Request body:
+	{
+	    "plan": "free"
+	}
+
+ logic
+	Overwrite the plan that you saved already with the one that is in this request.
+	This way Bitrise can update your addon if there was a plan change for any reason.
+*/
 func putProvision(w http.ResponseWriter, r *http.Request) {
-	//vars := mux.Vars(r)
-	//appSlug := vars["app_slug"]
+	decoder := json.NewDecoder(r.Body)
 
-	// Request body:
-	/*
-		{
-		    "plan": "free"
-		}
-	*/
+	var provisionData ProvisionData
+	err := decoder.Decode(&provisionData)
+	if err != nil {
+		http.Error(w, "Failed to parse request", 500)
+	}
+	provisionData.Username = mux.Vars(r)["app_slug"]
 
-	// logic
-	/*
-		Overwrite the plan that you saved already with the one that is in this request.
-		This way Bitrise can update your addon if there was a plan change for any reason.
-	*/
+	db, err := sql.Open("sqlite3", "./addon.db")
+	if err != nil {
+		http.Error(w, "Application error", 500)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("UPDATE users SET plan = ? WHERE username = ?")
+	if err != nil {
+		http.Error(w, "Failed to update user", 500)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(provisionData.Plan, provisionData.Username)
+	if err != nil {
+		http.Error(w, "Failed to update user", 500)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{}"))
 }
 
+/* deleteProvision
+logic
+	Delete the app's provisioned state, so the calls are pointed to this service
+	will be rejected in the Bitrise build.
+*/
 func deleteProvision(w http.ResponseWriter, r *http.Request) {
-	//vars := mux.Vars(r)
-	//appSlug := vars["app_slug"]
+	provisionData := ProvisionData{
+		Username: mux.Vars(r)["app_slug"],
+	}
+	fmt.Println(provisionData)
 
-	// logic
-	/*
-		Delete the app's provisioned state, so the calls are pointed to this service
-		will be rejected in the Bitrise build.
-	*/
+	db, err := sql.Open("sqlite3", "./addon.db")
+	if err != nil {
+		http.Error(w, "Application error", 500)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("DELETE FROM users WHERE username = ?")
+	if err != nil {
+		http.Error(w, "Failed to update user", 500)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(provisionData.Username)
+	if err != nil {
+		http.Error(w, "Failed to update user", 500)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{}"))
 }
